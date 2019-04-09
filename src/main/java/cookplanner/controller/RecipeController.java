b49b9c8226d9f7899f2ae0d95aeb78c7745f8ed2
Henk Keizer <hwkeizer@telfort.nl>
2019-04-09 17:00:51 +0200

    Added (limited) recipe update functionality

--- src/main/java/cookplanner/controller/RecipeController.java
+++ src/main/java/cookplanner/controller/RecipeController.java
@@ -5,11 +5,15 @@ import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import cookplanner.api.ApiResponse;
 import cookplanner.domain.Recipe;
+import cookplanner.domain.RecipeType;
 import cookplanner.exception.RecipeListEmptyException;
 import cookplanner.repository.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -52,6 +57,27 @@ public class RecipeController implements IApiResponse {
 	public @ResponseBody byte[] getImage(@RequestParam String name) throws IOException {
 		Path imagePath = FileSystems.getDefault().getPath(imageLocation, name);
 		return Files.readAllBytes(imagePath);
+	}
+	
+	@GetMapping("/types")
+	public ApiResponse<List<String>> getRecipeTypes() {
+		List<String> types = Stream.of(RecipeType.values())
+				.map(RecipeType::name)
+				.collect(Collectors.toList());
+		return createResponse(
+				200,
+				"Recept types succesvol opgehaald",
+				types);
+				
+	}
+	
+	@PutMapping("/update")
+	public ApiResponse<Recipe> updateRecipe(@RequestBody Recipe recipe) {
+		recipeRepository.save(recipe);
+		return createResponse(
+				200,
+				"Recept succesvol gewijzigd",
+				recipe);
 	}
 	
 }
